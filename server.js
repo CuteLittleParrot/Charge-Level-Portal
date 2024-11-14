8f73ccb62c8fe383a4514b0e0bd06e5fa4f54b9e
Zeenyk <federico05pese@gmail.com>
2024-11-14 12:06:16 +0100

    Fourth Try

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 
 // Serve static files (HTML, CSS, JavaScript)
 app.use(express.static('public'));
