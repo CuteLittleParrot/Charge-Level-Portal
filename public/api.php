6ebdcd98b3e54d7bd23425e1b5c96b9aa5ed0e16
Zeenyk <federico05pese@gmail.com>
2025-05-27 15:32:47 +0200

    Changed API

--- public/api.php
+++ public/api.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-$servername = "192.168.1.141";  // Host del database
+$servername = "localhost";  // Host del database
 $username = "root";              // Nome utente del database
 $password = "123";               // Password del database
 $dbname = "charge_level_portal"; // Nome del database
